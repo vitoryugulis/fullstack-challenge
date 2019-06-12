a726fef3d67ae51533272584bf02c71b8fdc907e
Vitor Yugulis <vitor.yugulis@gmail.com>
2019-06-12 17:46:44 -0300

    Add Angular material configuration module

---
+++ fullstack-challenge-angular/src/app/angular-material-configuration/angular-material-configuration.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCheckboxModule, MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule, MatCheckboxModule
+  ]
+})
+export class AngularMaterialConfigurationModule { }
--- fullstack-challenge-angular/src/app/app.module.ts
+++ fullstack-challenge-angular/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SpeciesComponent } from './components/species/species.component';
+import { AngularMaterialConfigurationModule } from './angular-material-configuration/angular-material-configuration.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SpeciesComponent } from './components/species/species.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule,
+    AngularMaterialConfigurationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
