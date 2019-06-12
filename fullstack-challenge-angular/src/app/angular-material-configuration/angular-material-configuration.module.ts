ed664c9adf0b6acab7308261bab200c7e484a6d5
Vitor Yugulis <vitor.yugulis@gmail.com>
2019-06-12 20:05:18 -0300

    Fix exports in angular configuration module

--- fullstack-challenge-angular/src/app/angular-material-configuration/angular-material-configuration.module.ts
+++ fullstack-challenge-angular/src/app/angular-material-configuration/angular-material-configuration.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCheckboxModule, MatButtonModule } from '@angular/material';
+import { MatCheckboxModule, MatButtonModule, MatTableModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule, MatCheckboxModule, MatTableModule
+  ],
+  exports: [
+    MatButtonModule, MatCheckboxModule, MatTableModule
   ]
 })
 export class AngularMaterialConfigurationModule { }
