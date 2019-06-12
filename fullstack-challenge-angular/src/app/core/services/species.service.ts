aacc4a784fc388b6518f2098231276cbaf3db03d
Vitor Yugulis <vitor.yugulis@gmail.com>
2019-06-12 20:06:06 -0300

    Fix getAllSpecies() to return an array of Species

--- fullstack-challenge-angular/src/app/core/services/species.service.ts
+++ fullstack-challenge-angular/src/app/core/services/species.service.ts
@@ -13,10 +13,10 @@ export class SpeciesService {
 
   constructor(private httpClient: HttpClient, private authorizationService: AuthorizationService) { }
 
-  async getAllSpecies(page : number = 0) : Promise<Species>{
+  async getAllSpecies(page : number = 0) : Promise<Species[]>{
     let httpHeaders = await this.setAuthorizationHeaders();
     let speciesUrl = `${environment.fullstack_challenge_api}species/?page=${page}`;
-    return await this.httpClient.get<Species>(speciesUrl, {headers:httpHeaders}).toPromise();
+    return await this.httpClient.get<Species[]>(speciesUrl, {headers:httpHeaders}).toPromise();
   }
 
   private async setAuthorizationHeaders() : Promise<HttpHeaders>{
